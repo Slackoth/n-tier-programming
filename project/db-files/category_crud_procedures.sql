f4eddb7efe5b2e6f58221f7e10df318b029425c7
Slackoth <00082318@uca.edu.sv>
2021-04-12 17:14:25 -0600

    Need to start again

---
+++ project/db-files/category_crud_procedures.sql
@@ -0,0 +1,47 @@
+--List category procedure
+CREATE PROC list_category
+AS
+SELECT category_id as ID, c_name  as Nombre, description as Descripcion, c_state as Estado
+FROM category ORDER BY category_id DESC
+GO;
+
+--Search category procedure
+CREATE PROC search_category
+@value VARCHAR(50) AS
+SELECT category_id as ID, c_name  as Nombre, description as Descripcion, c_state as Estado
+FROM category WHERE c_name LIKE '%' + @value + '%' 
+OR description LIKE '%' + @value + '%' ORDER BY c_name ASC 
+GO;
+
+--Insert category procedure
+CREATE PROC insert_category
+@name VARCHAR(50), @description VARCHAR(255) AS
+INSERT INTO category(c_name, description) VALUES(@name,@description)
+GO;
+
+--Update catgory procedure
+CREATE PROC update_category
+@id INT, @name VARCHAR(50), @description VARCHAR(255) AS
+UPDATE category SET c_name = @name, description = @description
+WHERE category_id = @id
+GO;
+
+--Delete category procedure
+CREATE PROC delete_category
+@id INT AS
+DELETE FROM category WHERE category_id = @id
+GO;
+
+--Activate category procedure
+CREATE PROC activate_category
+@id INT AS
+UPDATE category SET c_state = 1
+WHERE category_id = @id
+GO;
+
+--Deactivate category procedure
+CREATE PROC deactivate_category
+@id INT AS
+UPDATE category SET c_state = 0
+GO;
+
