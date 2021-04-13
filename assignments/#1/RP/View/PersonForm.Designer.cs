41d9226a5f2f0c689223b6f8322adfea1f501c36
Slackoth <00082318@uca.edu.sv>
2021-04-13 14:40:32 -0600

    Finished but needs revision

---
+++ assignments/#1/RP/View/PersonForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace View
+{
+    partial class PersonForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLastname = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.nupAge = new System.Windows.Forms.NumericUpDown();
+            this.dgvPerson = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nupAge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPerson)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 15);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(74, 22);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Nombre";
+            // 
+            // lblLastname
+            // 
+            this.lblLastname.AutoSize = true;
+            this.lblLastname.Location = new System.Drawing.Point(12, 51);
+            this.lblLastname.Name = "lblLastname";
+            this.lblLastname.Size = new System.Drawing.Size(81, 22);
+            this.lblLastname.TabIndex = 1;
+            this.lblLastname.Text = "Apellido";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(308, 15);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(51, 22);
+            this.lblAge.TabIndex = 2;
+            this.lblAge.Text = "Edad";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(308, 51);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(36, 22);
+            this.lblPhone.TabIndex = 3;
+            this.lblPhone.Text = "Tel";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(99, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(180, 30);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(99, 48);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(180, 30);
+            this.txtLastname.TabIndex = 5;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(365, 48);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(180, 30);
+            this.txtPhone.TabIndex = 6;
+            // 
+            // nupAge
+            // 
+            this.nupAge.Location = new System.Drawing.Point(365, 12);
+            this.nupAge.Minimum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.nupAge.Name = "nupAge";
+            this.nupAge.Size = new System.Drawing.Size(180, 30);
+            this.nupAge.TabIndex = 7;
+            this.nupAge.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // dgvPerson
+            // 
+            this.dgvPerson.AllowUserToAddRows = false;
+            this.dgvPerson.AllowUserToDeleteRows = false;
+            this.dgvPerson.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPerson.Location = new System.Drawing.Point(12, 84);
+            this.dgvPerson.Name = "dgvPerson";
+            this.dgvPerson.ReadOnly = true;
+            this.dgvPerson.RowHeadersWidth = 51;
+            this.dgvPerson.RowTemplate.Height = 29;
+            this.dgvPerson.Size = new System.Drawing.Size(776, 354);
+            this.dgvPerson.TabIndex = 8;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(608, 43);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(180, 30);
+            this.btnInsert.TabIndex = 9;
+            this.btnInsert.Text = "INSERTAR";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.BtnInsert_Click);
+            // 
+            // PersonForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dgvPerson);
+            this.Controls.Add(this.nupAge);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblLastname);
+            this.Controls.Add(this.lblName);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "PersonForm";
+            this.ShowIcon = false;
+            this.Text = "Tarea 1 - Programación N-Capas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nupAge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPerson)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLastname;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.NumericUpDown nupAge;
+        private System.Windows.Forms.DataGridView dgvPerson;
+        private System.Windows.Forms.Button btnInsert;
+    }
+}
+
---
+++ assignments/#1/RP/View/PersonForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using Business;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class PersonForm : Form
+    {
+        public PersonForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshDgvPerson();
+        }
+
+        private void BtnInsert_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string lastname = txtLastname.Text;
+            string phone = txtPhone.Text;
+            int age = Convert.ToInt32(nupAge.Value);
+            BPerson business = new();
+
+            business.Insert(name, lastname, age, phone);
+            RefreshDgvPerson();
+        }
+
+        private void RefreshDgvPerson()
+        {
+            BindingSource source = new();
+            dgvPerson.DataSource = source;
+            BPerson business = new();
+            source.DataSource = business.List();
+
+            // Resize the DataGridView columns to fit all available space.
+            dgvPerson.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+    }
+}
